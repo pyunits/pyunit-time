d17e21011e761c0bf77a0a994ed03f65ff9cad8d
jtyoui <jtyoui@qq.com>
2020-04-16 21:10:35 +0800

    重构字符串提取时间

---
+++ pyunit_time/retime/__init__.py
@@ -0,0 +1,6 @@
+# !/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time  : 2020/4/14 17:35
+# @Author: Jtyoui@qq.com
+# @Notes : 一个大型的正则抽取表达式
+
---
+++ pyunit_time/retime/hours.py
@@ -0,0 +1,5 @@
+# !/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time  : 2020/4/14 17:42
+# @Author: Jtyoui@qq.com
+# @Notes :
