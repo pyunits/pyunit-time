0ef099b73379be252a5fec4622def881d5b4e087
jtyoui <jtyoui@qq.com>
2024-07-03 15:34:22 +0800

    对月份修复 #15 错误

--- pyunit_time/retime/months.py
+++ pyunit_time/retime/months.py
@@ -52,17 +52,19 @@ class Months(IObserver):
         """处理上个月之类的
         比如处理： 上个月、上上个月、上3个月、2个月以前等等
         """
-        rule = r'([上下]+)个?月|[上下](\d+)个?月|(\d+)(?=个?月[以之]?[前后内])'
+        rule = r'([上下]+)个?月|[上下](\d+)个?月|(\d+)个?月[以之]?[前后内]'
         match = re.search(rule, self.key)
         if match:
-            if match.lastindex == 1:
-                length = len(match.group(1))
-            elif match.lastindex == 2:
-                length = int(match.group(2))
-            elif match.lastindex == 3:
-                length = int(match.group(3))
-                length = -length if '前' in match.group() else length
+            length = match.group(match.lastindex)
+            if not length.isdigit():
+                length = len(length)
             else:
-                return
-            length = -length if '上' in match.group() else length
+                length = int(length)
+
+            # 判断是否是之前的
+            for tip in ["上", "前"]:
+                value = match.group()
+                if tip in value:
+                    length = -length
+
             self.time = self.time.shift(months=length)
